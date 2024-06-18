110605b3b853dcccd87b784a2dbd26c4a46db125
Ondriy <u1mbrela96@gmail.com>
2024-06-18 15:18:02 +0500

    correct time

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ import telebot
 import os
 import datetime
 from datetime import datetime, timedelta
+import pytz
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 bot = telebot.TeleBot(os.environ.get('TOKEN_BOT'))
 
+SdTz = pytz.timezone('Asia/Yekaterinburg')
+
 from telebot import types
 
 def days_until_payday(payday_date):
-    today = datetime.now().date()
+    today = datetime.now(tz=SdTz).date()
     days_left = (payday_date.date() - today).days
     return max(days_left, 0)  # если зарплата уже сегодня, вернем 0, чтобы не было отрицательного значения
 
 def time_until_end_of_workday():
-    end_of_workday = datetime.now().replace(hour=17, minute=0, second=0, microsecond=0)  # предположим, что рабочий день заканчивается в 17:00
-    current_time = datetime.now()
+    current_time = datetime.now(tz=SdTz)
+    weekday = current_time.weekday()
+    if weekday > 4:
+        return "Выходной, спи"
+    print(weekday)
+    begin_of_workday = datetime.now(tz=SdTz).replace(hour=8, minute=0, second=0, microsecond=0)
+    if weekday < 4:
+        end_of_workday = datetime.now(tz=SdTz).replace(hour=17, minute=0, second=0, microsecond=0)  # рабочий день заканчивается в 17:00
+    else:
+        end_of_workday = datetime.now(tz=SdTz).replace(hour=16, minute=0, second=0, microsecond=0)  # рабочий день заканчивается в 16:00
+
+    if current_time < begin_of_workday:
+        return "Работа ещё не началась, спи"
+    if current_time > end_of_workday:
+        return "Работа уже кончилась, спи"
+
     time_left = end_of_workday - current_time
 
     hours_left = int(time_left.total_seconds() // 3600)
@@ -48,10 +65,10 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def start(message):
-    
+
     print(message.date)
     if message.text == 'Сколько до зарплаты?':
-        payday_date = datetime(2024, 6, 21)  # здесь нужно указать дату вашей зарплаты
+        payday_date = datetime(2024, 6, 21, tzinfo=SdTz)  # здесь нужно указать дату вашей зарплаты
         bot.send_message(message.from_user.id, "До зарплаты осталось {} дней".format(days_until_payday(payday_date)))
     elif message.text == 'Сколько до конца рабочего дня?':
         bot.send_message(message.from_user.id, time_until_end_of_workday())
